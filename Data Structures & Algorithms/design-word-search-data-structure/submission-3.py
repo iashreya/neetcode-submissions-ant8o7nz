9770934ff082bc15269b83d806fe05662c5f21b6
Shreya Anand <iashreya@hotmail.com>
2026-04-10 12:16:25 +0530

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,41 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = self.Node()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = self.Node()
+            node = node.children[ch]
+        node.EOW = True
+        return 
+
+    def search(self, word: str) -> bool:
+
+        def dfs(word, node):
+            if word == "":
+                if node.EOW:
+                    return True
+                else:
+                    return False
+
+            ch = word[0]
+            if ch == ".":
+                for child in node.children.values():
+                    found = dfs(word[1:], child)
+                    if found:
+                        return True
+                return False
+
+            if ch in node.children:
+                return dfs(word[1:], node.children[ch])
+            
+            return False
+        return dfs(word, self.root)
+
+    class Node:
+        def __init__(self):
+            self.children = {}
+            self.EOW = False
