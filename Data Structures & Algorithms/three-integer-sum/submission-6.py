b4548aca82acc44818f726046588a90df70d6837
Shreya Anand <iashreya@hotmail.com>
2026-04-15 21:02:09 +0530

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums, target, start):
+        h_map = defaultdict(list)
+        ans = []
+        for i in range(start, len(nums)):
+            if nums[i] in h_map:
+                for _ in h_map[nums[i]]:
+                    ans.append([_ , i])
+            
+            h_map[target-nums[i]].append(i)
+        return ans
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        
+        for i in range(len(nums)-2):
+            temp = self.twoSum(nums, -nums[i], i+1)
+            print(temp)
+            for j, k in temp:
+                ans.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+        
+        return list(ans)
+
+        
